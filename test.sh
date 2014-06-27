025e78ca270a5433d3d1515a477749818e2c1ac8
Bryan Davis <bd808@wikimedia.org>
2014-06-27 22:23:48 +0000

    Closer

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+TEST=( maintenance/benchmarks/benchmarkParse.php --wiki=enwiki NOFX )
+# FIXME: bug 66942
+EXTRA=( --config-value Eval.Jit=false )
+
+repeat () {
+    for i in {1..10}; do
+        $*
+    done
+}
+
+echo Testing mwscript...
+repeat mwscript "${TEST[@]}"
+
+echo Testing hhvm, RepoAuthoritative off...
+repeat ./hhscript "${TEST[@]}"
+
+echo Testing hhvm, RepoAuthoritative on with one MW version precompiled...
+repeat ./hhscript --config 1.24wmf10/config.hdf "${EXTRA[@]}" "${TEST[@]}"
+
+echo Testing hhvm, RepoAuthoritative on with two MW versions precompiled...
+repeat ./hhscript --config inuse/config.hdf "${EXTRA[@]}" "${TEST[@]}"
